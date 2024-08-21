adc54ea32d7941c7e57342275bc3b741e8eaa82c
Leroy <leroy.souz@gmail.com>
2024-08-20 23:50:02 -0400

    new prompts plus chat feature

--- message-sum.py
+++ message-sum.py
@@ -18,7 +18,47 @@ class MyBot(commands.Bot):
     
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+        
+    async def retrieve_messages(self, ctx, limit: int = 500, include_bot_messages: bool = True):
+        print(f"Retrieving last {limit} messages")
+        
+        messages = ctx.channel.history(limit=(limit + 1))
+        
+        message_history = [None]
+        async for message in messages:
+            if message.author.id != int(bot_user_id) or include_bot_messages:
+                message_history.insert(0, (message.author.global_name, message.content))
+        
+        message_history.pop(-1)
+        
+        
+        summary = """"""
+        for message in message_history:
+            if message is not None:
+                summary += f"{message[0]}: {message[1]}\n"
+        
+        return summary
     
+    async def on_message(self, message):
+        await self.process_commands(message)
+        
+        if self.user.mentioned_in(message):
+            history = await self.retrieve_messages(message)
+            
+            with open("chat_prompt.txt", "r") as f:
+                prompt = f.read()
+            
+            prompt += history
+            
+            prompt += f"\nQuestion posed: {message.content}"
+            
+            completion = client.chat.completions.create(model="gpt-4o-mini",
+                                                        messages=[
+                                                            {"role": "user", "content": prompt}
+                                                        ]
+                                                        )
+            
+            await message.channel.send(completion.choices[0].message.content)
     
 class Commands(commands.Cog):
     def __init__(self, bot):
@@ -26,24 +66,10 @@ class Commands(commands.Cog):
     
     @commands.command()
     async def summarize_chat(self, ctx, limit: int = 100):
-        print(f"Retrieving last {limit} messages")
-        messages = ctx.channel.history(limit=(limit + 1))
         
-        message_history = [None]
-        
-        async for message in messages:
-            if message.author.id != int(bot_user_id):
-                message_history.insert(0, (message.author.global_name, message.content))
-        
-        message_history.pop(-1)
-        
-        summary = """
-        """
-        for message in message_history:
-            if message is not None:
-                summary += f"{message[0]}: {message[1]}\n"
+        summary = await self.bot.retrieve_messages(ctx, limit, include_bot_messages=False)
                 
-        with open("prompt.txt", "r") as f:
+        with open("summarize_prompt.txt", "r") as f:
             prompt = f.read()
         
         prompt += summary
